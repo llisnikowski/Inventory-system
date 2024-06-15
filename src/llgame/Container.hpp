e83eeb47b918c3d90b96c57b170e223e34edc6e6
llisnikowski <l.lisnikowski@gmail.com>
2024-06-15 10:53:08 +0200

    Add iterators to container

--- src/llgame/Container.hpp
+++ src/llgame/Container.hpp
@@ -8,7 +8,11 @@ template<typename T>
 class Container
 {
 public:
-    using Slots = std::vector<T>;
+    using Slots                  = std::vector<T>;
+    using iterator               = typename Slots::iterator;
+    using const_iterator         = typename Slots::const_iterator;
+    using reverse_iterator       = typename Slots::reverse_iterator;
+    using const_reverse_iterator = typename Slots::const_reverse_iterator;
 
     explicit Container(std::size_t size);
 
@@ -17,6 +21,26 @@ public:
     std::size_t x, It it) -> decltype(std::declval<T>().setItem(it));
     T& getSlot(std::size_t x);
     const T& getSlot(std::size_t x) const;
+
+    Slots& getSlots() { return slots; }
+    const Slots& getSlots() const { return slots; }
+
+    std::size_t size() const { return slots.size(); }
+
+    // Iterators
+    iterator begin() { return slots.begin(); }
+    const_iterator begin() const { return slots.begin(); }
+    const_iterator cbegin() const { return slots.cbegin(); }
+    iterator end() { return slots.end(); }
+    const_iterator end() const { return slots.end(); }
+    const_iterator cend() const { return slots.cend(); }
+    // Reverse iterator
+    reverse_iterator rbegin() { return slots.rbegin(); }
+    const_reverse_iterator rbegin() const { return slots.rbegin(); }
+    const_reverse_iterator crbegin() const { return slots.crbegin(); }
+    reverse_iterator rend() { return slots.rend(); }
+    const_reverse_iterator rend() const { return slots.rend(); }
+    const_reverse_iterator crend() const { return slots.crend(); }
 
 private:
     Slots slots;
--- utest/Container_test.cpp
+++ utest/Container_test.cpp
@@ -36,3 +36,42 @@ TEST(Container_test, setItem)
     EXPECT_EQ(container.setItem(2, nullptr), item2);
     EXPECT_EQ(container.getSlot(2).getItem(), nullptr);
 }
+
+TEST(Container_test, getSlots)
+{
+    llgame::Container<SlotMock<ItemMock>> container(4);
+
+    EXPECT_EQ(container.size(), 4);
+
+    auto item1 = std::make_shared<ItemMock>();
+    EXPECT_EQ(container.setItem(2, item1), nullptr);
+    EXPECT_EQ(container.getSlots()[2].getItem(), item1);
+
+    const auto &ref = container;
+
+    EXPECT_EQ(&container.getSlots(), &ref.getSlots());
+}
+
+TEST(Container_test, iterators)
+{
+    llgame::Container<SlotMock<ItemMock>> container(4);
+    const auto &constContainer = container;
+
+    auto &slots            = container.getSlots();
+    const auto &constSlots = constContainer.getSlots();
+
+    EXPECT_EQ(container.begin(), slots.begin());
+    EXPECT_EQ(constContainer.begin(), constSlots.begin());
+    EXPECT_EQ(container.cbegin(), slots.cbegin());
+    EXPECT_EQ(container.end(), slots.end());
+    EXPECT_EQ(constContainer.end(), constSlots.end());
+    EXPECT_EQ(container.cend(), slots.cend());
+
+
+    EXPECT_EQ(container.rbegin(), slots.rbegin());
+    EXPECT_EQ(constContainer.rbegin(), constSlots.rbegin());
+    EXPECT_EQ(container.crbegin(), slots.crbegin());
+    EXPECT_EQ(container.rend(), slots.rend());
+    EXPECT_EQ(constContainer.rend(), constSlots.rend());
+    EXPECT_EQ(container.crend(), slots.crend());
+}
